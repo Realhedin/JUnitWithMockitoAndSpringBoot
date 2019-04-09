86d9734c28d69cdc9bab326404c868012488a7bb
dkorolev <ue73062@sbb.ch>
2019-04-09 16:40:28 +0300

    generic params added

---
+++ src/test/java/com/example/jUnitWithMockitoAndSpringBoot/business/ListMockTest.java
@@ -0,0 +1,45 @@
+package com.example.jUnitWithMockitoAndSpringBoot.business;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+
+/**
+ * @author dkorolev
+ *         Date: 4/9/2019
+ *         Time: 1:25 PM
+ */
+public class ListMockTest {
+
+    List mock = Mockito.mock(List.class);
+
+    @Test
+    public void test_basicSize() {
+        Mockito.when(mock.size()).thenReturn(5);
+        Assert.assertEquals(5, mock.size());
+    }
+
+    @Test
+    public void test_mockDifferentValues() {
+        Mockito.when(mock.size()).thenReturn(5).thenReturn(10);
+        Assert.assertEquals(5, mock.size());
+        Assert.assertEquals(10, mock.size());
+    }
+
+
+    @Test
+    public void test_withParameters() {
+        Mockito.when(mock.get(0)).thenReturn("Test1");
+        Assert.assertEquals("Test1", mock.get(0));
+        Assert.assertEquals(null, mock.get(1));
+    }
+
+    @Test
+    public void test_withGenericParameters() {
+        Mockito.when(mock.get(ArgumentMatchers.anyInt())).thenReturn("Test1");
+        Assert.assertEquals("Test1", mock.get(ArgumentMatchers.anyInt()));
+    }
+}

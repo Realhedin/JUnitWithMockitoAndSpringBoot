2341cc4e57532291ba97a83f2e793a0b477aa61e
dkorolev <ue73062@sbb.ch>
2019-04-09 19:05:36 +0300

    adding spying mechanism

--- src/test/java/com/example/jUnitWithMockitoAndSpringBoot/business/ListMockTest.java
+++ src/test/java/com/example/jUnitWithMockitoAndSpringBoot/business/ListMockTest.java
@@ -2,10 +2,13 @@ package com.example.jUnitWithMockitoAndSpringBoot.business;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -19,65 +22,94 @@ import org.mockito.Mockito;
  */
 public class ListMockTest {
 
-    List<String> mock = Mockito.mock(List.class);
+    List<String> mockList = Mockito.mock(List.class);
 
     @Test
     public void test_basicSize() {
-        when(mock.size()).thenReturn(5);
-        assertEquals(5, mock.size());
+        when(mockList.size()).thenReturn(5);
+        assertEquals(5, mockList.size());
     }
 
     @Test
     public void test_mockDifferentValues() {
-        when(mock.size()).thenReturn(5).thenReturn(10);
-        assertEquals(5, mock.size());
-        assertEquals(10, mock.size());
+        when(mockList.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mockList.size());
+        assertEquals(10, mockList.size());
     }
 
 
     @Test
     public void test_withParameters() {
-        when(mock.get(0)).thenReturn("Test1");
-        assertEquals("Test1", mock.get(0));
-        assertEquals(null, mock.get(1));
+        when(mockList.get(0)).thenReturn("Test1");
+        assertEquals("Test1", mockList.get(0));
+        assertEquals(null, mockList.get(1));
     }
 
     @Test
     public void test_withGenericParameters() {
-        when(mock.get(anyInt())).thenReturn("Test1");
-        assertEquals("Test1", mock.get(anyInt()));
+        when(mockList.get(anyInt())).thenReturn("Test1");
+        assertEquals("Test1", mockList.get(anyInt()));
     }
 
     @Test
     public void verificationBasics() {
         //SUT (System Under Test - part of testing system)
-        String value = mock.get(0);
-        String value2 = mock.get(1);
+        String value = mockList.get(0);
+        String value2 = mockList.get(1);
         //verify
-        verify(mock).get(0);
-        verify(mock, Mockito.times(2)).get(anyInt());
-        verify(mock, Mockito.atLeast(1)).get(anyInt());
+        verify(mockList).get(0);
+        verify(mockList, Mockito.times(2)).get(anyInt());
+        verify(mockList, Mockito.atLeast(1)).get(anyInt());
     }
 
     @Test
     public void capturingBacics() {
         //SUT
-        mock.add("TestForCapturing");
+        mockList.add("TestForCapturing");
         //verify
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(mock).add(captor.capture());
+        verify(mockList).add(captor.capture());
         assertEquals("TestForCapturing",captor.getValue());
     }
 
     @Test
     public void capturingListValues() {
         //SUT
-        mock.add("TestForCapturing");
-        mock.add("TestForCapturing2");
+        mockList.add("TestForCapturing");
+        mockList.add("TestForCapturing2");
         //verify
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(mock, times(2)).add(captor.capture());
+        verify(mockList, times(2)).add(captor.capture());
         assertEquals("TestForCapturing",captor.getAllValues().get(0));
         assertEquals("TestForCapturing2",captor.getAllValues().get(1));
+    }
+
+    @Test
+    public void beforeSpyingUseMocks() {
+        List list = mock(ArrayList.class);
+        System.out.println(list.get(1)); //null
+        System.out.println(list.size()); //0
+        list.add("Test1");
+        list.add("Test2");
+        System.out.println(list.size()); //0
+        when(list.size()).thenReturn(5);
+        System.out.println(list.size()); //5
+    }
+
+    @Test
+    //spy - create object with real class behavior
+    //plus we can use when().thenReturn()
+    public void afterSpyingUseSpy() {
+        List list = spy(ArrayList.class);
+        list.add("Test0");
+        System.out.println(list.get(0)); //Test0
+        System.out.println(list.size()); //1
+        list.add("Test1");
+        list.add("Test2");
+        System.out.println(list.size()); //3
+        when(list.size()).thenReturn(5);
+        System.out.println(list.size()); //5
+        //verify
+        verify(list).add("Test1");
     }
 }
